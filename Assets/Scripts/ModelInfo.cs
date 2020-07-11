4edbda48c5dc05cba41fcb417ad4e95194d2e549
Androix777 <androix77@gmail.com>
2020-07-11 15:24:43 +0500

    Reworked stand manager

---
+++ Assets/Scripts/ModelInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelInfo : MonoBehaviour
+{
+    [System.Serializable]
+    public class ModelPart
+    {
+        public GameObject modelPart;
+        public string name;
+        public string note;
+    }
+    public string modelName;
+    public string modelNote;
+    public ModelPart [] modelParts;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/StandManager.cs
+++ Assets/Scripts/StandManager.cs
@@ -6,23 +6,18 @@ using UnityEngine.UI;
 public class StandManager : MonoBehaviour
 {
     public float rotationSpeed = 2;
-    [System.Serializable]
-    public class Model
-    {
-        public GameObject model;
-        public string name;
-        public string note;
-    }
-    public Model [] models;
+    public GameObject [] models;
     public GameObject canvas, element, currentModel;
     private Transform content;
     private Text note;
+    private ModelInfo[] modelInfo;
     void Start()
     {
-        int i = 0;
+        modelInfo = new ModelInfo [models.Length];
         content = canvas.transform.Find("LeftPanel").Find("Scroll").Find("Content");
         note = canvas.transform.Find("DownPanel").GetChild(0).GetComponent<Text>();
-        foreach(Model model in models)
+        int i = 0;
+        foreach(GameObject model in models)
         {
             AddElement(model, i);
             i++;
@@ -36,18 +31,20 @@ public class StandManager : MonoBehaviour
             currentModel.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * rotationSpeed, 0));
         }
     }
-    private void AddElement(Model model, int i)
+
+    private void AddElement(GameObject model, int i)
     {
+        modelInfo[i] = model.transform.GetComponent<ModelInfo>();
         Transform newElementTransform = Instantiate(element).transform;
         newElementTransform.SetParent(content, false);
-        newElementTransform.GetChild(0).gameObject.GetComponent<Text>().text = model.name;
+        newElementTransform.GetChild(0).gameObject.GetComponent<Text>().text = modelInfo[i].modelName;
         newElementTransform.GetComponent<Button>().onClick.AddListener(delegate{ShowModel(i);});
     }
 
     private void ShowModel(int i)
     {
-        note.text = models[i].note;
+        note.text = modelInfo[i].modelNote;
         Destroy(currentModel);
-        currentModel = Instantiate(models[i].model);
+        currentModel = Instantiate(models[i]);
     }
 }
